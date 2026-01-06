6efbc5589d26ba8c47ea9966a07dba3a98ee15a1
Aditya Raj <adityarajpathak08@gmail.com>
2026-01-06 12:16:29 +0530

    deleted: file.txt

--- To-Do list.py
+++ To-Do list.py
@@ -2,10 +2,10 @@ tasks = []
 
 # function to display the available option
 def display_menu():
-    print("\n1. Add Task")
-    print("2. Edit Task")
-    print("3. Delete Task")
-    print("4. Exit")
+    print("\n1. Add Task🤺")
+    print("2. Edit Task👨‍🏫")
+    print("3. Delete Task😶‍🌫️")
+    print("4. Exit🫣")
 
 # start an infinite loop to continuously interact with the user
 while True:
@@ -28,4 +28,44 @@ while True:
             # Display the current tasks with their indices 
             for index, task in enumerate(tasks):
                 print(f"{index + 1}. {task}")
-                  
+            # prompt the user to enter the index of the task to edit
+            try:
+                task_index = int(input("Enter task index to edit: ")) - 1
+                if task_index < 0 or task_index >= len(tasks):
+                    print("Invalid index.")
+                else:
+                    new_task = input("Enter new task: ")
+                    tasks[task_index] = new_task
+                    print("Task updated successfully.")
+            except ValueError:
+                print("Please enter a valid number.")
+        else:
+            print("NO tasks available to edit.")
+    # option 3: Delete Task
+    elif choice == '3':
+        if tasks:
+            # display the current tasks with their indices
+            for index, task in enumerate(tasks):
+                print(f"{index + 1}. {task}")
+            # prompt the user to enter the index of the task to delete
+            try:
+                task_index = int(input("Enter task index to delete: ")) -1
+                if task_index < 0 or task_index >= len(tasks):
+                    print("Invalid index.")
+                else:
+                    removed = tasks.pop(task_index)
+                    print(f"Task '{removed}' deleted successfully.")
+            except ValueError:
+                print("Please enter a valid number.")
+        else:
+            print("NO tasks available to delete.")
+
+    # option 4: Exit
+    elif choice == '4':
+        # Exit the application
+        print("Exiting the application. Goodbye!") 
+        break
+    # handle invalid menu choices
+    else:
+        print("Invalid choice. Please select a valid option.")
+
